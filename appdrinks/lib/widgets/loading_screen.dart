db810ce06016ffaee69484b80f1ed016e24bb976
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-07 16:47:21 -0300

    Adiciona tela de carregamento com indicador circular e fundo vermelho

---
+++ appdrinks/lib/widgets/loading_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 204, 7, 17), // Vermelho Netflix
+      child: const Center(
+        child: CircularProgressIndicator(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
