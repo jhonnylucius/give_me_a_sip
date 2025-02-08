72c3dcca489441a6a5628a842460041271cd0247
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-08 02:03:05 -0300

    Adiciona registro do CocktailRepository no serviço de localização para gerenciamento de cocktails

--- appdrinks/lib/services/locator_service.dart
+++ appdrinks/lib/services/locator_service.dart
@@ -1,11 +1,16 @@
 import 'package:app_netdrinks/controller/cocktail_list_controller.dart';
+import 'package:app_netdrinks/models/cocktail.dart';
 import 'package:app_netdrinks/models/cocktail_api.dart';
+import 'package:app_netdrinks/repository/cocktail_repository.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
   getIt.registerLazySingleton<CocktailApi>(() => CocktailApi());
+  getIt.registerLazySingleton<CocktailRepository>(() => CocktailRepository(
+      getIt<CocktailApi>(), Hive.box<Cocktail>('cocktailBox')));
   getIt.registerLazySingleton<CocktailListController>(
-      () => CocktailListController());
+      () => CocktailListController(repository: getIt<CocktailRepository>()));
 }
