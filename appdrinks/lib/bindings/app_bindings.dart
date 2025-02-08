bf9132e34a974570a13039266ed67c94001643da
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-08 02:01:56 -0300

    Adiciona gerenciamento de dependências para CocktailListController e CocktailController

--- appdrinks/lib/bindings/app_bindings.dart
+++ appdrinks/lib/bindings/app_bindings.dart
@@ -1,17 +1,21 @@
 import 'package:app_netdrinks/controller/cocktail_detail_controller.dart';
+import 'package:app_netdrinks/controller/cocktail_list_controller.dart';
 import 'package:app_netdrinks/models/cocktail.dart';
 import 'package:app_netdrinks/models/cocktail_api.dart';
 import 'package:app_netdrinks/repository/cocktail_repository.dart';
+import 'package:app_netdrinks/services/locator_service.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 
-class AppBindings extends Bindings {
+class AppBindings implements Bindings {
   @override
   void dependencies() {
-    final api = CocktailApi();
-    final cocktailBox = Hive.box<Cocktail>('cocktailBox');
-    final repository = CocktailRepository(api, cocktailBox);
-
-    Get.put(CocktailController(repository));
+    Get.lazyPut(() => CocktailRepository(
+          getIt<CocktailApi>(),
+          Hive.box<Cocktail>('cocktailBox'),
+        ));
+    Get.lazyPut(() => CocktailListController(repository: Get.find()));
+    // Adicione esta linha
+    Get.lazyPut(() => CocktailController(Get.find()));
   }
 }
