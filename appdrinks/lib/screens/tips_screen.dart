e663dc19f78ad942f1b5cf033b6e32e7be716228
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-08 15:41:17 -0300

    Atualiza a construção do widget TipsScreen para utilizar a sintaxe mais concisa na passagem de chave

--- appdrinks/lib/screens/tips_screen.dart
+++ appdrinks/lib/screens/tips_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TipsScreen extends StatelessWidget {
-  const TipsScreen({Key? key}) : super(key: key);
+  const TipsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
