3aee956275d9c362910d1eae6d71cfd020f32c47
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-08 23:55:46 -0300

    Nenhuma alteração de código foi realizada.

--- appdrinks/lib/screens/register_screen.dart
+++ appdrinks/lib/screens/register_screen.dart
@@ -2,6 +2,7 @@ import 'package:app_netdrinks/screens/verify_email_screen.dart';
 import 'package:app_netdrinks/services/auth_services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart'; // Importe o GetX
 
 class RegisterScreen extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
@@ -10,8 +11,9 @@ class RegisterScreen extends StatelessWidget {
       TextEditingController();
   final TextEditingController _nomeController = TextEditingController();
   final AuthService _authService = AuthService();
+  final _isLoading = false.obs; // Adicionar esta linha
 
-  RegisterScreen({Key? key}) : super(key: key);
+  RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,111 +36,136 @@ class RegisterScreen extends StatelessWidget {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(16.0),
                 ),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'assets/Icon-192.png',
-                      width: 90,
-                      height: 90,
-                    ),
-                    const SizedBox(height: 8.0),
-                    TextField(
-                      controller: _nomeController,
-                      style: const TextStyle(color: Colors.black),
-                      decoration: const InputDecoration(labelText: 'Nome'),
-                    ),
-                    const SizedBox(height: 8.0),
-                    TextField(
-                      controller: _emailController,
-                      style: const TextStyle(color: Colors.black),
-                      decoration: const InputDecoration(labelText: 'E-mail'),
-                    ),
-                    const SizedBox(height: 8.0),
-                    TextField(
-                      obscureText: true,
-                      controller: _senhaController,
-                      style: const TextStyle(color: Colors.black),
-                      decoration: const InputDecoration(labelText: 'Senha'),
-                    ),
-                    const SizedBox(height: 8.0),
-                    TextField(
-                      obscureText: true,
-                      controller: _confirmarSenhaController,
-                      style: const TextStyle(color: Colors.black),
-                      decoration:
-                          const InputDecoration(labelText: 'Confirmar Senha'),
-                    ),
-                    const SizedBox(height: 8.0),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (_senhaController.text ==
-                            _confirmarSenhaController.text) {
-                          final result = await _authService.cadastrarUsuario(
-                            email: _emailController.text,
-                            senha: _senhaController.text,
-                            nome: _nomeController.text,
-                            context: context,
-                          );
+                child: Obx(() => _isLoading.value
+                    ? Container(
+                        color: Colors
+                            .transparent, //fundo transparente para o conteiner do progressIndicator
+                        child: const Center(
+                          child: CircularProgressIndicator(
+                            color: Color.fromARGB(255, 204, 7, 17),
+                          ),
+                        ),
+                      )
+                    : Column(
+                        children: [
+                          Image.asset(
+                            'assets/Icon-192.png',
+                            width: 90,
+                            height: 90,
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextField(
+                            controller: _nomeController,
+                            style: const TextStyle(color: Colors.black),
+                            decoration:
+                                const InputDecoration(labelText: 'Nome'),
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextField(
+                            controller: _emailController,
+                            style: const TextStyle(color: Colors.black),
+                            decoration:
+                                const InputDecoration(labelText: 'E-mail'),
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextField(
+                            obscureText: true,
+                            controller: _senhaController,
+                            style: const TextStyle(color: Colors.black),
+                            decoration:
+                                const InputDecoration(labelText: 'Senha'),
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextField(
+                            obscureText: true,
+                            controller: _confirmarSenhaController,
+                            style: const TextStyle(color: Colors.black),
+                            decoration: const InputDecoration(
+                                labelText: 'Confirmar Senha'),
+                          ),
+                          const SizedBox(height: 8.0),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (_senhaController.text ==
+                                  _confirmarSenhaController.text) {
+                                _isLoading.value = true; // Ativar loading
 
-                          if (result == null) {
-                            // Cadastro bem-sucedido. Obtém o usuário autenticado.
-                            final user = FirebaseAuth.instance.currentUser;
+                                try {
+                                  final result =
+                                      await _authService.cadastrarUsuario(
+                                    email: _emailController.text,
+                                    senha: _senhaController.text,
+                                    nome: _nomeController.text,
+                                    context: context,
+                                  );
 
-                            if (user != null) {
-                              // Navega para a tela de verificação de e-mail, passando o usuário.
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      VerifyEmailScreen(user: user),
-                                ),
-                              );
-                            } else {
-                              // Lidar com o caso em que o usuário é nulo após o cadastro (improvável, mas possível)
-                              if (context.mounted) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text(
-                                        'Erro ao obter usuário após o cadastro.'),
-                                    backgroundColor: Colors.red,
-                                  ),
-                                );
+                                  if (result == null) {
+                                    // Cadastro bem-sucedido. Obtém o usuário autenticado.
+                                    final user =
+                                        FirebaseAuth.instance.currentUser;
+
+                                    if (user != null) {
+                                      // Navega para a tela de verificação de e-mail, passando o usuário.
+                                      if (context.mounted) {
+                                        Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                VerifyEmailScreen(user: user),
+                                          ),
+                                        );
+                                      }
+                                    } else {
+                                      // Lidar com o caso em que o usuário é nulo após o cadastro (improvável, mas possível)
+                                      if (context.mounted) {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          const SnackBar(
+                                            content: Text(
+                                                'Erro ao obter usuário após o cadastro.'),
+                                            backgroundColor: Colors.red,
+                                          ),
+                                        );
+                                      }
+                                    }
+                                  } else {
+                                    // Houve um erro no cadastro.
+                                    if (context.mounted) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          content: Text(result),
+                                          backgroundColor: Colors.red,
+                                        ),
+                                      );
+                                    }
+                                  }
+                                } finally {
+                                  _isLoading.value = false; // Desativar loading
+                                }
+                              } else {
+                                // Senhas não coincidem.
+                                if (context.mounted) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Senhas não conferem.'),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  );
+                                }
                               }
-                            }
-                          } else {
-                            // Houve um erro no cadastro.
-                            if (context.mounted) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(result),
-                                  backgroundColor: Colors.red,
-                                ),
-                              );
-                            }
-                          }
-                        } else {
-                          // Senhas não coincidem.
-                          if (context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Senhas não conferem.'),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-                        }
-                      },
-                      child: const Text('Cadastrar'),
-                    ),
-                    const SizedBox(height: 8.0),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/login');
-                      },
-                      child: const Text('Já tenho uma conta!'),
-                    ),
-                  ],
-                ),
+                            },
+                            child: const Text('Cadastrar'),
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/login');
+                            },
+                            child: const Text('Já tenho uma conta!'),
+                          ),
+                        ],
+                      )),
               ),
             ],
           ),
