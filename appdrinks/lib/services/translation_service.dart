e4a77851963a0a2b5d8509b4f1301d4f87a8387b
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-11 05:57:51 -0300

    refatora serviço de tradução para carregar dados de ingredientes e drinks dinamicamente

--- appdrinks/lib/services/translation_service.dart
+++ appdrinks/lib/services/translation_service.dart
@@ -1,36 +1,73 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
 class TranslationService extends GetxService {
-  Map<String, String> en = {};
-  Map<String, String> pt = {};
-  Map<String, String> es = {};
+  static TranslationService get to => Get.find();
 
-  Future<TranslationService> init() async {
-    // Carregar arquivos de tradução
-    final enJson = await rootBundle.loadString('assets/lang/en.json');
-    final ptJson = await rootBundle.loadString('assets/lang/pt.json');
-    final esJson = await rootBundle.loadString('assets/lang/es.json');
+  final _ingredientsMap = <String, Map<String, String>>{};
+  final _drinksData = <String, Map<String, dynamic>>{};
+  final _interfaceStrings = <String, Map<String, String>>{};
+  final _currentLanguage = 'pt'.obs;
 
-    en = Map<String, String>.from(json.decode(enJson));
-    pt = Map<String, String>.from(json.decode(ptJson));
-    es = Map<String, String>.from(json.decode(esJson));
-
-    return this;
+  Future<void> initialize() async {
+    await Future.wait([
+      _loadIngredientsMap(),
+      _loadDrinksData(),
+      _loadInterfaceStrings(),
+    ]);
   }
 
-  String translate(String key, {String? locale}) {
-    locale ??= Get.locale?.languageCode;
+  Future<void> _loadIngredientsMap() async {
+    final file =
+        await rootBundle.loadString('assets/data/ingredients_map.json');
+    final Map<String, dynamic> data = json.decode(file);
+    data.forEach((key, value) {
+      _ingredientsMap[key] = Map<String, String>.from(value);
+    });
+  }
 
-    switch (locale) {
-      case 'pt':
-        return pt[key] ?? key;
-      case 'es':
-        return es[key] ?? key;
-      default:
-        return en[key] ?? key;
+  Future<void> _loadDrinksData() async {
+    final file = await rootBundle.loadString('assets/data/drinks_data.json');
+    final Map<String, dynamic> data = json.decode(file);
+    data.forEach((key, value) {
+      _drinksData[key] = Map<String, dynamic>.from(value);
+    });
+  }
+
+  Future<void> _loadInterfaceStrings() async {
+    for (var lang in ['en', 'pt', 'es']) {
+      final file = await rootBundle.loadString('assets/lang/$lang.json');
+      final Map<String, dynamic> data = json.decode(file);
+      _interfaceStrings[lang] = Map<String, String>.from(data);
     }
+  }
+
+  Future<void> setLanguage(String langCode) async {
+    try {
+      _currentLanguage.value = langCode;
+      await Get.updateLocale(Locale(langCode));
+    } catch (e) {
+      Logger().e('Erro ao definir idioma: $e');
+      rethrow;
+    }
+  }
+
+  String translateIngredient(String ingredient) {
+    return _ingredientsMap[ingredient]?[_currentLanguage.value] ?? ingredient;
+  }
+
+  String translateDrinkField(String drinkId, String field) {
+    return _drinksData[drinkId]?[field]?[_currentLanguage.value] ??
+        _drinksData[drinkId]?[field]?['en'] ??
+        _drinksData[drinkId]?[field] ??
+        '';
+  }
+
+  String getInterfaceString(String key) {
+    return _interfaceStrings[_currentLanguage.value]?[key] ?? key;
   }
 }
