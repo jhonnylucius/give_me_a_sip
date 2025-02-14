ba1ab8c72846220aed09a38e303ccae703e49260
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-14 02:24:44 -0300

    adiciona repositório de ranking para gerenciamento de drinks e likes

---
+++ appdrinks/lib/repository/ranking_repository.dart
@@ -0,0 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logger/logger.dart';
+
+import '../models/cocktail.dart';
+import '../models/drink_likes.dart';
+import '../repository/cocktail_repository.dart';
+
+class RankingRepository {
+  final FirebaseFirestore _firestore;
+  final Logger _logger;
+  final CocktailRepository _cocktailRepository;
+
+  RankingRepository({
+    FirebaseFirestore? firestore,
+    Logger? logger,
+    required CocktailRepository cocktailRepository,
+  })  : _firestore = firestore ?? FirebaseFirestore.instance,
+        _logger = logger ?? Logger(),
+        _cocktailRepository = cocktailRepository;
+
+  Future<List<(Cocktail, DrinkLikes)>> getTopDrinks({int limit = 10}) async {
+    try {
+      _logger.d('Iniciando busca de top drinks...');
+
+      final allCocktails = await _cocktailRepository.getAllCocktails();
+      _logger.d('Total de cocktails carregados: ${allCocktails.length}');
+
+      final likesSnapshot = await _firestore
+          .collection('drinks_likes')
+          .orderBy('total_likes', descending: true)
+          .limit(limit)
+          .get();
+
+      _logger
+          .d('Documentos encontrados no ranking: ${likesSnapshot.docs.length}');
+      List<(Cocktail, DrinkLikes)> rankedDrinks = [];
+
+      for (var doc in likesSnapshot.docs) {
+        try {
+          final drinkId = doc.id;
+          final cocktail = allCocktails.firstWhere(
+            (c) => c.idDrink == drinkId,
+          );
+
+          final drinkLikes = DrinkLikes.fromJson({
+            'drinkId': drinkId,
+            'total_likes': doc.data()['total_likes'] ?? 0,
+            'users_liked': doc.data()['users_liked'] ?? [],
+          });
+
+          rankedDrinks.add((cocktail, drinkLikes));
+          _logger.d(
+              'Drink adicionado: ${cocktail.name} com ${drinkLikes.totalLikes} likes');
+        } catch (e) {
+          _logger.e('Erro ao processar drink ${doc.id}: $e');
+          continue;
+        }
+      }
+
+      _logger.d('Total de drinks rankeados: ${rankedDrinks.length}');
+      return rankedDrinks;
+    } catch (e) {
+      _logger.e('Erro ao buscar ranking de drinks: $e');
+      throw Exception('Falha ao carregar ranking: $e');
+    }
+  }
+}
