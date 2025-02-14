583aedc5da63b45870973f9dd55cab837e096042
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-14 02:25:55 -0300

    adiciona widget RankingCard para exibir detalhes de drinks em ranking

---
+++ appdrinks/lib/widgets/ranking_card.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../contants/ranking_contants.dart';
+import '../models/cocktail.dart';
+import '../models/drink_likes.dart';
+import '../widgets/shared/widgets/heart_display.dart';
+
+class RankingCard extends StatelessWidget {
+  final Cocktail cocktail;
+  final DrinkLikes drinkLikes;
+  final int position;
+
+  const RankingCard({
+    super.key,
+    required this.cocktail,
+    required this.drinkLikes,
+    required this.position,
+  });
+
+  Color _getMedalColor() {
+    switch (position) {
+      case 1:
+        return Color(
+            int.parse('0xFF${RankingConstants.topOneColor.substring(1)}'));
+      case 2:
+        return Color(
+            int.parse('0xFF${RankingConstants.topTwoColor.substring(1)}'));
+      case 3:
+        return Color(
+            int.parse('0xFF${RankingConstants.topThreeColor.substring(1)}'));
+      default:
+        return Colors.grey;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Get.toNamed(
+        '/cocktail-detail',
+        arguments: {
+          'id': cocktail.idDrink,
+          'name': cocktail.name,
+          'imageUrl': cocktail.imageUrl,
+        },
+      ),
+      child: Container(
+        height: RankingConstants.cardHeight,
+        decoration: BoxDecoration(
+          color: Colors.black54,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: _getMedalColor(),
+            width: 2,
+          ),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: RankingConstants.rankingImageSize,
+              height: RankingConstants.rankingImageSize,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: NetworkImage(cocktail.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '#$position ${cocktail.name}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: _getMedalColor(),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  HeartDisplay(
+                    likes: drinkLikes.totalLikes,
+                    color: _getMedalColor(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
