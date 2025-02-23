f1897c92694010722fbf9ef17b147de75739965c
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-23 01:08:54 -0300

    adicionar componente RankingCard para exibir coquetéis com informações de likes e popularidade

--- appdrinks/lib/screens/drink_ranking_screen.dart
+++ appdrinks/lib/screens/drink_ranking_screen.dart
@@ -3,6 +3,9 @@ import 'package:get/get.dart';
 
 import '../contants/ranking_contants.dart';
 import '../controller/ranking_controller.dart';
+import '../models/cocktail.dart';
+import '../models/drink_likes.dart';
+import '../screens/cocktail_detail_screen.dart';
 import '../widgets/ranking_list.dart';
 
 class RankingScreen extends GetView<RankingController> {
@@ -62,6 +65,102 @@ class RankingScreen extends GetView<RankingController> {
 
           return RankingList(drinks: controller.rankedDrinks);
         }),
+      ),
+    );
+  }
+}
+
+class RankingCard extends StatelessWidget {
+  final Cocktail cocktail;
+  final DrinkLikes drinkLikes;
+  final int rank;
+
+  const RankingCard({
+    super.key,
+    required this.cocktail,
+    required this.drinkLikes,
+    required this.rank,
+  });
+
+  Color _getMedalColor() {
+    switch (rank) {
+      case 1:
+        return Colors.yellow;
+      case 2:
+        return Colors.grey;
+      case 3:
+        return Colors.orange;
+      default:
+        return Colors.transparent;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Get.to(() => CocktailDetailScreen(cocktail: cocktail)),
+      child: Container(
+        height: RankingConstants.cardHeight,
+        decoration: BoxDecoration(
+          color: Colors.black54,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: _getMedalColor(),
+            width: 2,
+          ),
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ),
+              child: Image.asset(
+                cocktail.getDrinkImageUrl(), // Usando o mesmo método da home
+                width: RankingConstants.rankingImageSize,
+                height: RankingConstants.rankingImageSize,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    color: Colors.grey[900],
+                    width: RankingConstants.rankingImageSize,
+                    height: RankingConstants.rankingImageSize,
+                    child: const Icon(Icons.error, color: Colors.redAccent),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '$rank. ${cocktail.name}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'Likes: ${drinkLikes.totalLikes}',
+                      style: TextStyle(color: Colors.grey[400]),
+                    ),
+                    Text(
+                      'Popularidade: ${drinkLikes.likePercentage.toStringAsFixed(1)}%',
+                      style: TextStyle(color: Colors.grey[400]),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
