6062ce2336126101e431721dbaf5564510554c89
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-03-15 01:02:16 -0300

    habilitar suporte edge-to-edge na MainActivity

--- appdrinks/android/app/src/main/kotlin/com/luciano/appdrinks/MainActivity.kt
+++ appdrinks/android/app/src/main/kotlin/com/luciano/appdrinks/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.luciano.appdrinks
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import androidx.core.view.WindowCompat
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        // Habilita edge-to-edge
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
